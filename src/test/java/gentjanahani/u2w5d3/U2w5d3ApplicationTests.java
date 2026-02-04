ef03e28876e2aca5a639a552f44054086d04e8c4
Gentjana <hanigentjana@gmail.com>
2026-02-04 15:28:13 +0100

    add_testCalcolaTot

--- src/test/java/gentjanahani/u2w5d3/U2w5d3ApplicationTests.java
+++ src/test/java/gentjanahani/u2w5d3/U2w5d3ApplicationTests.java
@@ -1,15 +1,37 @@
 package gentjanahani.u2w5d3;
 
+import gentjanahani.u2w5d3.entities.Order;
+import gentjanahani.u2w5d3.entities.Pizza;
+import gentjanahani.u2w5d3.entities.Table;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class U2w5d3ApplicationTests {
 
-    @ParameterizedTest
+
+    @Autowired
+    private AnnotationConfigApplicationContext ctx;
+
+    @Test
     void testCalcolaTotale() {
+        Table tavolo = (Table) ctx.getBean("tavolo1");
+        Order ordineTest = new Order(tavolo, 2);
+        ordineTest.addItems((Pizza) ctx.getBean("margherita"));
+        ordineTest.addItems((Pizza) ctx.getBean("margherita"));
+
+        double tot = ordineTest.calcolaTotale();
+        assertEquals(12.98, tot);
+
 
     }
+
 
 }
