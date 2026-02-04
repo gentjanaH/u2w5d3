1b32d68c0c52dcaa402cd59c2bb9078a659deaf8
Gentjana <hanigentjana@gmail.com>
2026-02-04 15:44:52 +0100

    add_tests
    test_libera e assegna tavolo
    test_sull'eccezione

--- src/test/java/gentjanahani/u2w5d3/U2w5d3ApplicationTests.java
+++ src/test/java/gentjanahani/u2w5d3/U2w5d3ApplicationTests.java
@@ -2,6 +2,7 @@ package gentjanahani.u2w5d3;
 
 import gentjanahani.u2w5d3.entities.Order;
 import gentjanahani.u2w5d3.entities.Pizza;
+import gentjanahani.u2w5d3.entities.StatoTavolo;
 import gentjanahani.u2w5d3.entities.Table;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class U2w5d3ApplicationTests {
@@ -33,5 +35,30 @@ class U2w5d3ApplicationTests {
 
     }
 
+
+    @Test
+    void testAssegnaTavolo() {
+        Table tavolo = (Table) ctx.getBean("tavolo1");
+        Order ordineTest = new Order(tavolo, 2);
+        ordineTest.assegnaTavolo(tavolo);
+
+        assertEquals(StatoTavolo.OCCUPATO, tavolo.getStatoTavolo());
+    }
+
+    @Test
+    void testLiberaTavolo() {
+        Table tavolo = (Table) ctx.getBean("tavolo1");
+        Order ordineTest = new Order(tavolo, 2);
+
+        ordineTest.chiudiOrdine();
+        assertEquals(StatoTavolo.LIBERO, tavolo.getStatoTavolo());
+    }
+
+    @Test
+    void testExeption() {
+        Table tavolo = (Table) ctx.getBean("tavolo1");
+
+        assertThrows(RuntimeException.class, () -> new Order(tavolo, 5));
+    }
 
 }
